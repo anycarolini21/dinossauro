73010ea4bd27d6dcc888b71445da637c88e02693
carol <anycarolinif340@gmail.com>
2024-11-12 11:48:33 -0300

    fazendo fundo

--- dinossauro/MainPage.xaml.cs
+++ dinossauro/MainPage.xaml.cs
@@ -5,7 +5,7 @@ public partial class MainPage : ContentPage
 	bool estaMorto = false;
 	bool estaPulando = false;
 	const int tempoEntreFrames = 25;
-	int velocidade1 = 0;
+	int velocidade1 = 3;
 	int velocidade2 = 0;
 	int velocidade3 = 0;
 	int velocidade = 0;
@@ -16,7 +16,7 @@ public partial class MainPage : ContentPage
 
 	public MainPage()
 	{
-		InitializeComponent();
+		InitializeComponent();	
 	}
 
 	protected override void OnSizeAllocated(double w, double h)
@@ -27,62 +27,64 @@ public partial class MainPage : ContentPage
 	}
 	void CalculaVelocidade(double w)
 	{
-		velocidade1 = (int)(w * 0.001);
+		velocidade1 = (int)(w * 0.004);
 		velocidade2 = (int)(w * 0.004);
 		velocidade3 = (int)(w * 0.008);
 		velocidade = (int)(w * 0.01);
 
 	}
-	void CorrigeTamanhoCenario (double w, double h)
+	void CorrigeTamanhoCenario(double w, double h)
 	{
 		foreach (var A in HSLayer1.Children)
-		(A as Image).WidthRequest = w;
-		foreach (var A in HSLayer2.Children)
-		(A as Image).WidthRequest = w;
-		foreach (var A in HSLayer3.Children)
-		(A as Image).WidthRequest = w;
-		
-		HSLayer1.WidthRequest = w * 1.5;
-		HSLayer2.WidthRequest = w * 1.5;
-		HSLayer3.WidthRequest = w * 1.5;
+			(A as Image).WidthRequest = w;
+		foreach (var A in floresta2.Children)
+			(A as Image).WidthRequest = w;
+		foreach (var A in chao3.Children)
+			(A as Image).WidthRequest = w;
+
+		HSLayer1.WidthRequest = w;
+		floresta2.WidthRequest = w;
+		chao3.WidthRequest = w;
 	}
+
 	void GerenciaCenarios()
 	{
 		MoveCenario();
 		GerenciaCenario(HSLayer1);
-		GerenciaCenario(HSLayer2);
-		GerenciaCenario(HSLayer3);
-	    
+		GerenciaCenario(floresta2);
+		GerenciaCenario(chao3);
+
 	}
 	void MoveCenario()
 	{
-		HSLayer1.TranslationX-=velocidade2;
-		HSLayer2.TranslationX-=velocidade1;
-		HSLayer3.TranslationX-=velocidade;
-		
+		HSLayer1.TranslationX -= velocidade2;
+		floresta2.TranslationX -= velocidade1;
+		chao3.TranslationX -= velocidade;
+
 	}
 	void GerenciaCenario(HorizontalStackLayout hsl)
 	{
 		var view = (hsl.Children.First() as Image);
-		if (view.WidthRequest+hsl.TranslationX<0)
+		if (view.WidthRequest + hsl.TranslationX < 0)
 		{
 			hsl.Children.Remove(view);
-            hsl.Children.Add(view);
-			hsl.TranslationX=view.TranslationX;
+			hsl.Children.Add(view);
+			hsl.TranslationX = view.TranslationX;
 		}
 	}
 	async Task Desenha()
 	{
-		while(! estaMorto)
+		while (!estaMorto)
 		{
 			GerenciaCenarios();
 			await Task.Delay(tempoEntreFrames);
+			
 		}
 	}
 	protected override void OnAppearing()
-		{
-			base.OnAppearing();
-			Desenha();
-		}
+	{
+		base.OnAppearing();
+		Desenha();
+	}
 
 }
